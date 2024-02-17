77b6102a99513152cfedbc10992ad503168da00f
Richard Tingle <6330028+richardTingle@users.noreply.github.com>
2024-02-17 17:25:48 +0000

    Tests are all functional currently, remove unit

--- plugin/src/test/java/com/onemillionworlds/TypedMaterialsPluginTest.java
+++ plugin/src/test/java/com/onemillionworlds/TypedMaterialsPluginTest.java
@@ -9,15 +9,5 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class TypedMaterialsPluginTest {
-    @Test
-    void pluginRegisterDefaultTasks() {
-        // Create a test project and apply the plugin
-        Project project = ProjectBuilder.builder().build();
-        project.getPlugins().apply("java");
-        project.getPlugins().apply("com.onemillionworlds.typedmaterials");
 
-        // Verify the default tasks are registered
-        assertNotNull(project.getTasks().findByName("core"));
-        assertNotNull(project.getTasks().findByName("effects"));
-    }
 }
